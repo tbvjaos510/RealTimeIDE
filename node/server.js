b7048996050c989745232ea46218a025cebae947
gudwlsg1 <4587991@naver.com>
2018-04-14 16:06:29 +0900

    test
    test

--- node/server.js
+++ node/server.js
@@ -1,1 +1,9 @@
-var app = require('expres');
+var app = require('exprees');
+
+app.get('/',function(req,res){
+    res.send('Hello');
+});
+
+app.listen('3000',function(){
+    console.log('Connected 3000 port!');
+});
