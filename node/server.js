8fc39be789373817d4f16141cf527a467e04302c
gudwlsg1 <4587991@naver.com>
2018-04-14 16:09:06 +0900

    test
    test

--- node/server.js
+++ node/server.js
@@ -1,7 +1,12 @@
-var app = require('exprees');
+var express = require('express');
+var app = express();
 
 app.get('/',function(req,res){
     res.send('Hello');
+});
+
+app.get('/dgsw',function(req,res){
+    res.send('서상희 병신');
 });
 
 app.listen('3000',function(){
