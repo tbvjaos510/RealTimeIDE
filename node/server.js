74bcbcc2f73195d869b0662ddcfe253a19ece176
gudwlsg1 <4587991@naver.com>
2018-04-14 16:11:10 +0900

    1111
    11111

--- node/server.js
+++ node/server.js
@@ -9,6 +9,14 @@ app.get('/dgsw',function(req,res){
     res.send('서상희 병신');
 });
 
+app.get('/ang',function(req,res){
+    res.send(`<html>
+    <head>
+    <script>alert('1');</script>
+    </head>
+    </html>`)
+});
+
 app.listen('3000',function(){
     console.log('Connected 3000 port!');
 });
