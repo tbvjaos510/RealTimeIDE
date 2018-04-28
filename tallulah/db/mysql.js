80625de97e47bb840d01269708e82c3139a9e0cd
tbvjaos510 <tbvjaos510@naver.com>
2018-04-28 17:02:16 +0900

    회원가입 mysql

--- tallulah/db/mysql.js
+++ tallulah/db/mysql.js
@@ -21,19 +21,38 @@ Ta_mysql.test = function(callback){
 };
 Ta_mysql.login = function(id, pw, callback){
     connection.query("select password, name, user_ident from t_users where email_id = ?",[id], function(err, results){
-        if (err) callback({success : false, message : "알 수 없는 오류"});
+        if (err) callback({status : 1, success : false, message : "알 수 없는 오류"});
 
         if (results[0] == null){
-            callback({success : false, message : "아이디가 틀렸습니다."});
+            callback({status : 2, success : false, message : "아이디가 틀렸습니다."});
         }
         else if (results[0].password === pw){
-            callback({success : true, message : "로그인 성공", name:results[0].name, ident:results[0].user_ident});
+            callback({status : 3, success : true, message : "로그인 성공", name:results[0].name, ident:results[0].user_ident});
         }
         else{
-            callback({success : false, message : "비밀번호가 틀렸습니다."});
+            callback({status : 4, success : false, message : "비밀번호가 틀렸습니다."});
         }
     });
 };
+Ta_mysql.signup = function(id, pw, name, callback){
+    Ta.mysql.login(id, pw, function(results){
+        if (results.status == 2){
+            connection.query("insert into t_users (email_id, password, name) values ('?', '?', '?')", [id, pw, name], function(err, results){
+                if (err) callback({status : 2, success : false, message : "알 수 없는 오류"});
+                else{
+                    callback({status : 3, success : true, messsage:"성공"});
+                }
+            });
+            
+
+        }else if(results.status === 4 || results.status === 3){
+            callback({status : 1, success : false, message : "해당 아이디가 존재합니다."});
+        }
+        else{
+            callback({status : 2, success : false, message : "알 수 없는 오류"});
+        }
+    });
+}
 
 
 module.exports = Ta_mysql;
