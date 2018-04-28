09fa038d2993a4dbfb1205771ff121395a1b9775
gudwlsg1 <4587991@naver.com>
2018-04-28 15:45:28 +0900

    login

--- tallulah/routes/login.js
+++ tallulah/routes/login.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended : false}));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
