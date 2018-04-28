6e908c27c8633aff5c174cd24231cc1150d8c9b5
tbvjaos510 <tbvjaos510@naver.com>
2018-04-28 16:12:23 +0900

    login 테스트

---
+++ tallulah/db/mysql.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var Ta_mysql = {};
+var connection = mysql.createConnection({
+    host : '114.108.167.90',
+    port : '3306',
+    user : 'dgsw',
+    password : 'dnrhddltks',
+    database : 'dgsw_sms',
+});
+Ta_mysql.connect = function(){
+    connection.connect();
+};
+Ta_mysql.test = function(callback){
+
+    connection.query("select * from city", function(err, results){
+        if (err) throw err;
+        
+        callback(results);
+    });
+
+};
+
+module.exports = Ta_mysql;
--- tallulah/routes/login.js
+++ tallulah/routes/login.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var mysql = require('../db/mysql');
+mysql.connect();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send("login page");
+  mysql.test(function (results){
+      res.send(results);
+  });
 });
 
 module.exports = router;
