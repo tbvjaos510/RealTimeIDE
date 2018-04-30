7ded8e824a3419b7683d9cb2ceebd92cd3c07f0c
tbvjaos510 <tbvjaos510@naver.com>
2018-04-30 13:21:55 +0900

    잡다한거 수정

--- tallulah/db/auth.js
+++ tallulah/db/auth.js
@@ -1,32 +1,29 @@
 var mysql = require('mysql');
-var Ta_mysql = {};
+var auth = {};
 
 var connection = mysql.createConnection({
-    host : '114.108.167.90',
-    port : '3306',
-    user : 'dgsw',
-    password : 'dnrhddltks',
-    database : 'dgsw_sms',
+  host: '114.108.167.90',
+  port: '3306',
+  user: 'dgsw',
+  password: 'dnrhddltks',
+  database: 'dgsw_sms'
 });
 /**
  * mysql서버에 연결합니다.
- */
-Ta_mysql.connect = function(){
-    connection.connect();
-};
-Ta_mysql.test = function(callback){
-   
-    connection.query("select * from city", function(err, results){
-        if (err) throw err;
-        
-        callback(results);
-    });
+ */ auth.connect = function () {
+  connection.connect();
+}; auth.test = function (callback) {
+  connection.query('select * from city', function (err, results) {
+    if (err) throw err;
+
+    callback(results);
+  });
 
 };
 
 /**
  * @typedef loginCallback
- * @type {object} 
+ * @type {object}
  * @property {number} status 결과 상태 (1 : DB오류, 2 : 아이디가 틀림, 3. 비밀번호가 틀림, 4. 성공)
  * @property {boolean} success 성공 여부
  * @property {String} message 오류(성공) 설명
@@ -41,32 +38,30 @@ Ta_mysql.test = function(callback){
  * @param {String} name
  * @param {(data:loginCallback)=>void} callback
  * @returns {null}
- */
-Ta_mysql.login = function(id, pw, callback){
-    connection.query("select password, name, user_ident from t_users where email_id = ?",[id], function(err, results){
-        if (err) callback({status : 1, success : false, message : "알 수 없는 오류"});
+ */ auth.login = function (id, pw, callback) {
+  connection.query('select password, name, user_ident from t_users where email_id = ?', [id], function (err, results) {
+    if (err) callback({ status: 1, success: false, message: '알 수 없는 오류' });
 
-        if (results[0] == null){
-            callback({status : 2, success : false, message : "아이디가 틀렸습니다."});
-        }
-        else if (results[0].password === pw){
-            callback({status : 4, success : true, message : "로그인 성공", name:results[0].name, ident:results[0].user_ident});
-        }
-        else{
-            callback({status : 3, success : false, message : "비밀번호가 틀렸습니다."});
-        }
-    });
+    if (results[0] == null) {
+      callback({ status: 2, success: false, message: '아이디가 틀렸습니다.' });
+    } else if (results[0].password === pw) {
+      callback({ status: 4, success: true, message: '로그인 성공', name: results[0].name, ident: results[0].user_ident });
+    } else {
+      callback({ status: 3, success: false, message: '비밀번호가 틀렸습니다.' });
+    }
+  });
 };
 
 /**
  * @typedef signupCallback
- * @type {object} 
+ * @type {object}
  * @property {number} status 결과 상태 (1 : 아이디 중복, 2 : DB 오류, 3. 성공)
  * @property {boolean} success 성공 여부
- * @property {String} message 오류(성공) 설명 
+ * @property {String} message 오류(성공) 설명
  */
 
 /**
+ * @description
  * mysql User table에 유저 정보를 추가합니다.
  * @param {String} id
  * @param {String} pw
@@ -74,28 +69,26 @@ Ta_mysql.login = function(id, pw, callback){
  * @param {(data:signupCallback)=>void} callback
  * @returns {null}
  */
-Ta_mysql.signup = function(id, pw, name, callback){
-    Ta_mysql.login(id, pw, function(results){
-        if (results.status == 2){
-            connection.query("insert into t_users (email_id, password, name) values (?, ?, ?)", [id, pw, name], function(err, results){
-                if (err) {
-                    callback({status : 2, success : false, message : "알 수 없는 오류"});
-                    console.log(err.message);
-                }
-                else{
-                    callback({status : 3, success : true, messsage:"성공"});
-                }
-            });
-            
+auth.signup = function (id, pw, name, callback) {
+  auth.login(id, pw, function (results) {
+    if (results.status == 2) {
+      connection.query('insert into t_users (email_id, password, name) values (?, ?, ?)', [id, pw, name], function (err, results) {
+        if (err) {
+          callback({ status: 2, success: false, message: '알 수 없는 오류' });
+          console.log(err.message);
+        } else {
+          callback({ status: 3, success: true, messsage: '성공' });
+        }
+      });
 
-        }else if(results.status === 4 || results.status === 3){
-            callback({status : 1, success : false, message : "해당 아이디가 존재합니다."});
-        }
-        else{
-            callback({status : 2, success : false, message : "알 수 없는 오류"});
-        }
-    });
+
+    } else if (results.status === 4 || results.status === 3) {
+      callback({ status: 1, success: false, message: '해당 아이디가 존재합니다.' });
+    } else {
+      callback({ status: 2, success: false, message: '알 수 없는 오류' });
+    }
+  }
+  );
 };
 
-
-module.exports = Ta_mysql;
+module.exports = auth;
