4b1aaba03e09f3e302fd8334635b4669221f5c9b
tbvjaos510 <tbvjaos510@naver.com>
2018-04-30 12:29:03 +0900

    이름 변경

--- tallulah/app.js
+++ tallulah/app.js
@@ -16,7 +16,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- tallulah/routes/login.js
+++ tallulah/routes/login.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('../db/mysql');
+var mysql = require('../db/auth');
 
 mysql.connect();
 
