bac6d4bde2cbbe5ab08addf4919f15abdaa2bcd7
mango906 <minkb01@dgsw.hs.kr>
2018-05-26 12:13:17 +0900

    1

---
+++ test/경빈/socket/mainsock.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {SocketIO.Server} io 
+ */
+
+module.exports = function(io){
+    io.on('connection', function(socket){
+        console.log('Socket Connected : ' + socket.id);
+        socket.on('data', function(data){
+            console.log('data : '+ data);
+            io.emit('data', data);
+        });
+    });
+};
