f87159984e01e5502692223f24cde41958e9684e
vhrleo0904 <vhrleo0904@naver.com>
2018-05-26 15:42:07 +0900

    베운거

--- test/성혁/socket/mainsock.js
+++ test/성혁/socket/mainsock.js
@@ -5,10 +5,20 @@
  */
 module.exports = function(io){
     io.on('connection',function(socket){
+        socket.inroom = 'room1';
+        socket.join(socket.inroom);
+        socket.on('room', function(data) {
+            socket.leave(socket.inroom);
+            console.log('inroom' + data);
+            socket.inroom = data;
+            socket.join(data);
+        });
+
         console.log('Socket Connected : ' + socket.id);
+
         socket.on('data',function(data){
             console.log('data : ' + data);
-            io.emit('data', data);
+            io.to(socket.inroom).emit('data', data);
         });
     });
 };
