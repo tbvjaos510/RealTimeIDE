7e7b4015edd40f1cfc3cf69e5d055a55fab7845b
bjj3036 <baebo0823@naver.com>
2018-05-26 15:40:27 +0900

    WebSocket-배준재-완성

--- test/배준재/socket/mainsock.js
+++ test/배준재/socket/mainsock.js
@@ -4,11 +4,20 @@
  */
 
 module.exports=function(io){
-    io.on('connection',function(socket){
+    io.on('connection',function(socket){//성공적으로 연결되었을시
         console.log('socket connected: '+socket.id);
+        socket.inroom=1;
+        socket.join(socket.inroom);
+        socket.on('room',function(data){
+            console.log('Entering Room to '+data+'....');
+            socket.leave(socket.inroom);
+            socket.inroom=data;
+            socket.join(socket.inroom);
+        });
+
         socket.on('data',function(data){
             console.log('Socket ID:'+this.id+' data :'+data);
-            io.emit('data',data);
+            io.to(socket.inroom).emit('data',data);
         })
     });
 };
