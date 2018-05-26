1e48b2202580668e4899ae5d451b31bdbeaa83ce
tbvjaos510 <tbvjaos510@naver.com>
2018-05-26 15:39:22 +0900

    io.of 설명

--- test/상희/app.js
+++ test/상희/app.js
@@ -3,15 +3,25 @@ var app = require('express')();
 app.get('/', function(req, res){
     res.send('Hello World');
 });
+
+
 app.get('/socket', function(req, res){
     res.sendFile(__dirname + "/main.html");
 });
-
-
+app.get('/socket/room1', function(req, res){
+    res.sendFile(__dirname + "/room1.html");
+});
+app.get('/socket/room2', function(req, res){
+    res.sendFile(__dirname + "/room2.html");
+});
+app.get('/socket/room3', function(req, res){
+    res.sendFile(__dirname + "/room3.html");
+});
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-require('./socket/mainsock')(io);
+require('./socket/controlSock')(io);
+//require('./socket/mainsock')(io);
 
 server.listen(3000, function() {
   console.log('Socket IO server listening on port 3000');
