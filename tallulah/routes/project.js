5bc1891965274830f3b70f2cf13ec486e58fd3d3
서상희 <tbvjaos510@naver.com>
2018-07-15 17:29:48 +0900

    프로젝트 가입 추가

--- tallulah/routes/project.js
+++ tallulah/routes/project.js
@@ -10,55 +10,104 @@ router.get('/', function (req, res, next) {
 router.post('/create', function (req, res) {
   if (req.isAuthenticated()) {
     if (!req.body.name || !req.body.desc || !req.body.private)
-      return res.send({ status: -1, success: false, message: '인자값이 전달되지 않았습니다.' });
-      project.create(req.body.name, req.session.passport.user.ident, req.body.desc, req.bodyy.private, function (data) {
+      return res.send({
+        status: -1,
+        success: false,
+        message: '인자값이 전달되지 않았습니다.'
+      });
+    project.create(req.body.name, req.session.passport.user.ident, req.body.desc, req.bodyy.private, function (data) {
       return res.send(data);
     });
 
-  }
-  else
-    res.send({ status: 0, success: false, message: '로그인 되지 않았습니다.' });
+  } else
+    res.send({
+      status: 0,
+      success: false,
+      message: '로그인 되지 않았습니다.'
+    });
+});
+
+router.post('/insert', function (req, res) {
+  if (req.isAuthenticated()) {
+    if (!req.body.pid)
+      return res.send({
+        status: -1,
+        success: false,
+        message: '인자값이 전달되지 않았습니다.'
+      });
+    project.insert(req.session.passport.user.ident, req.body.pid, 1, function (data) {
+      return res.send(data);
+    });
+  } else
+    return res.send({
+      status: 0,
+      success: false,
+      message: '로그인 되지 않았습니다.'
+    });
 });
 
 router.post('/delete', function (req, res) {
   if (req.isAuthenticated()) {
     if (!req.body.ident) {
-      return res.send({ status: -1, success: false, message: '인자값이 전달되지 않았습니다.' });
+      return res.send({
+        status: -1,
+        success: false,
+        message: '인자값이 전달되지 않았습니다.'
+      });
     }
     project.delete(req.body.ident, req.session.passport.user.ident, function (data) {
       return res.send(data);
     });
-  }
-  else {
-    return res.send({ status: 0, success: false, message: '로그인 되지 않았습니다.' });
+  } else {
+    return res.send({
+      status: 0,
+      success: false,
+      message: '로그인 되지 않았습니다.'
+    });
   }
 });
 
-router.post('/update',function(req,res){
-  if(req.isAuthenticated()){
-    if(!req.body.ident || !req.body.name){
-      return res.send({status : -1, success : false, message : "인자값이 전달되지 않았습니다."});
+router.post('/update', function (req, res) {
+  if (req.isAuthenticated()) {
+    if (!req.body.ident || !req.body.name) {
+      return res.send({
+        status: -1,
+        success: false,
+        message: "인자값이 전달되지 않았습니다."
+      });
     }
-    project.update(req.session.passport.user.ident, req.body.ident, req.body.name, req.body.desc,function(data){
+    project.update(req.session.passport.user.ident, req.body.ident, req.body.name, req.body.desc, function (data) {
       return res.send(data);
     })
-  }else{
-    return res.send({status : -1, success : false, message : "로그인이 되지 않았습니다."});
+  } else {
+    return res.send({
+      status: -1,
+      success: false,
+      message: "로그인이 되지 않았습니다."
+    });
   }
 });
 
-router.post('/search', function(req, res){
-  if (req.isAuthenticated()){
+router.post('/search', function (req, res) {
+  if (req.isAuthenticated()) {
     if (!req.body.keyword)
-    return res.send({status : -1, success : false, message : "인자값이 전달되지 않았습니다."});
-    else{
-      
-    project.search(req.body.keyword,function(data){
-      return res.send(data);
-    })
+      return res.send({
+        status: -1,
+        success: false,
+        message: "인자값이 전달되지 않았습니다."
+      });
+    else {
+
+      project.search(req.body.keyword, function (data) {
+        return res.send(data);
+      })
     }
-  }else{
-    return res.send({status : -1, success : false, message : "로그인이 되지 않았습니다."});
+  } else {
+    return res.send({
+      status: -1,
+      success: false,
+      message: "로그인이 되지 않았습니다."
+    });
   }
 });
 
@@ -68,9 +117,12 @@ router.post('/get', function (req, res) {
     project.select(req.session.passport.user.ident, function (data) {
       res.send(data);
     });
-  }
-  else
-    return res.send({ status: 0, success: false, message: '로그인 되지 않았습니다.' });
+  } else
+    return res.send({
+      status: 0,
+      success: false,
+      message: '로그인 되지 않았습니다.'
+    });
 });
 
 
