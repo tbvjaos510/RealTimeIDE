776455bb5a73013dce76b3590287f7b1e5c0f3ea
gudwlsg1 <4587991@naver.com>
2018-07-15 16:20:44 +0900

    socket
    메신져

--- tallulah/socket/onlinecode.js
+++ tallulah/socket/onlinecode.js
@@ -1,9 +1,11 @@
 
 var srouter = require('./socket_nsp');
+var mrouter = require('./message');
 /**
  * 
  * @param {SocketIO.Server} io 
  */
 module.exports = function (io) {
     srouter(io, '/main');
+    mrouter(io, '/chat');
 };
