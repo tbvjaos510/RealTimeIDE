a15982be3b9a0bbefe928bf37763fd4076dda9e5
서상희 <tbvjaos510@naver.com>
2018-07-15 17:34:34 +0900

    invite 추가

--- tallulah/routes/project.js
+++ tallulah/routes/project.js
@@ -45,6 +45,24 @@ router.post('/insert', function (req, res) {
       message: '로그인 되지 않았습니다.'
     });
 });
+router.post('/invite', function (req, res) {
+  if (req.isAuthenticated()) {
+    if (!req.body.pid || !req.body.uid)
+      return res.send({
+        status: -1,
+        success: false,
+        message: '인자값이 전달되지 않았습니다.'
+      });
+    project.insert(req.body.uid, req.body.pid, 1, function (data) {
+      return res.send(data);
+    });
+  } else
+    return res.send({
+      status: 0,
+      success: false,
+      message: '로그인 되지 않았습니다.'
+    });
+});
 
 router.post('/delete', function (req, res) {
   if (req.isAuthenticated()) {
