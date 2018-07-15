88fc1329c85869575463e2ffaab77eacbc418bc7
mango906 <minkb01@dgsw.hs.kr>
2018-07-15 16:27:10 +0900

    search

--- tallulah/public/js/front/createTree.js
+++ tallulah/public/js/front/createTree.js
@@ -5,8 +5,6 @@ function makeTree(selector) {
     this.tree = new orangeTree(selector);
 }
 
-
-
 makeTree.prototype.makeDefault = function () {
     // $(this.selector).find(".tree").empty();
         var this1 = this;
@@ -86,6 +84,17 @@ makeTree.prototype.addProject = function (projectData, descData) {
     }
     
 };
+
+makeTree.prototype.searchProject = function(projectData){
+    var projectData = $("input[name='search']").val();
+    var projects = $.ajax({
+        url: "project/search",
+        method: "POST",
+        async: false
+    }).responseJSON.data;
+
+    console.log(projects);
+}
 
 
 
